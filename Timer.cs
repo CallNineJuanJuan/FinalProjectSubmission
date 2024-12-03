5528b8b3e86b758daaa77c360b35f02e0609d7ab
Mel <159973209+SpaceLilith@users.noreply.github.com>
2024-12-03 16:28:48 -0500

    Update Timer.cs

--- Timer.cs
+++ Timer.cs
@@ -11,37 +11,37 @@ public class Timer : MonoBehaviour
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public GameEnding gameEnding;
 
-    bool m_IsPlayerAtExit;
-    bool m_IsPlayerCaught;
     float m_Timer;
     [SerializeField] TextMeshProUGUI timerText;
-    [SerializeField] float remainingTime;
+    [SerializeField] float remainingTime = 30;
+
     void Update()
     {
-        if (remainingTime > 0)
-        {
-            remainingTime -= Time.deltaTime;
-        }
-        else if (remainingTime <= 0)
-        {
-            if (m_IsPlayerAtExit == true)
-            {
-                Debug.Log("Player reached the exit!");
-                EndLevel(exitBackgroundImageCanvasGroup, false);
-            }
-            else
-            {
-                remainingTime = 0;
-                Debug.Log("Player failed to reach the exit.");
-                timerText.color = Color.red;
-                EndLevel(caughtBackgroundImageCanvasGroup, true);
-            }
-        }
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+            if(gameEnding.m_IsPlayerAtExit)
+            {
+                Debug.Log("Player reached the exit!");
+                timerText.text = "Exited!";
+                remainingTime = 1;
+            }
+        }
+        else if (remainingTime <= 0)
+        {
+            remainingTime = 0;
+            Debug.Log("Player failed to reach the exit.");
+            timerText.color = Color.red;
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
+        }
     }
+
     public void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
